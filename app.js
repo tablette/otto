b71f0a315a064be8cf71a544ec242773dd0c39c6
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-07-24 12:26:34 -0400

    edit per Udemy Vue.JS lec 9

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 new Vue({ // note is or looks like object
     el: '#app', // element / just a css selector
     data: {
-        tagline: 'help users sniff out the content they need',
-        name: 'User Userson',
+        title: "otto",
+        tagline: "make finding content easy",
+        apology: "Sorry, we haven't learned that trick yet.",
         username: 'Username',
         password: 'Password',
+        name: 'User Userson',
+        numberOfLabels: 0,
         url: 'https://www.iainstitute.org/what-is-ia',
         classes: ['one', 'two']
     },
@@ -19,7 +22,18 @@ new Vue({ // note is or looks like object
             } else {
                 tod = 'evening';
             }
-            return `Hello and good ${tod} to you, dear ${this.name}!`
+            return `Hello and good ${tod} to you!`
+        },
+        changeNumberOfLabels(number) {
+            this.numberOfLabels += number
+            if (this.numberOfLabels < 0) {
+                alert("That's not possible.")
+                this.numberOfLabels = 0;
+            }
+        },
+        confirm(){
+            alert(`Got it! You have ${this.numberOfLabels} labels.`) // instead, append to DOM if button clicked
+            
         }
     }
 })
